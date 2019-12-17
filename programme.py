9ca9be0df7f4ac15a24b7ad390ffc86a6a522713
ArashBazdar <bazdar.arash@gmail.com>
2019-12-17 12:19:52 +0100

    ajout nom

--- programme.py
+++ programme.py
@@ -1,10 +1,7 @@
 print("Bonjour")
 print("Ludovic")
-<<<<<<< HEAD
 print("fama")
 print("Bazdar")
-=======
 print("BOUGUERRA")
 print("fama")
 print("modif Lova")
->>>>>>> 622a47bed424e8e73c3d7906449140604ff2cf23
